90097a7f1a7aaeebe4e6a19ef751437531b74ec1
JuniperRhy <Gwyniver.Rhy@gmail.com>
2021-12-16 11:47:37 -0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
+const path = require("path");
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
